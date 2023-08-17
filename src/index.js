a39b6ec3abc14ea032acb45b6628e64b646480a6
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-17 19:49:32 -0300

    instalado o sucrase e a extenção thunder client

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => response.status(200).json("Hello, pessoal!"));
-app.listen(8080, () => console.log("Servidor rodando na porta 8080"));
+app.get("/", (request, response) =>
+  response.status(200).json("Hello, pessoal!")
+);
+
+const port = 3333;
+app.listen(port, () => console.log("Servidor rodando na porta 8080"));
