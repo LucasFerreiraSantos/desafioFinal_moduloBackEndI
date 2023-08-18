35ccef809a7f8ddc66cc66d01d0c8b449b7f9c4d
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-17 23:56:14 -0300

    adicionado o readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Desafio final
+
+## Módulo Back-end I
+
+Neste módulo vamos aprender sobre
+lógica de programação, javascript, API
+Rest, NodeJS, Express, libs e como subir
+uma aplicação back-end em produção.
+O objetivo desta avaliação é verificar se
+vamos atingir os conhecimentos
+propostos para este módulo.
+
+### O que vamos fazer?
+** Aplicação Lista de Recados **
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
 
 const app = express();
-
-app.get("/", (request, response) =>
-  response.status(200).json("Hello, pessoal!")
-);
-
 const port = 3333;
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/", (req, res) => ({}));
+
+app.post("/users", (req, res) => {
+  co;
+});
+
+app.put("users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name: updatedName } = req.body;
+
+  const user = users.find((user) => user.id === Number(id));
+
+  user.name = updatedName;
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuário não encontrado!",
+    });
+  }
+});
+
 app.listen(port, () => console.log("Servidor rodando na porta 8080"));
