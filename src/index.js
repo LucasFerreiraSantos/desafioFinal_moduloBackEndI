732ff89ce57122eefc36ef3362cda5b67cb98c0a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-22 20:24:48 -0300

    acertado o arquivo nodemon sucrase

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
-const express = require("express");
+//Importando a biblioteca EXPRESS
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
-const port = 3333;
+let myuuid = uuidv4();
 
+//Convertendo o EXPRESS em formato JSON
 app.use(express.json());
 
-const users = [];
+app.get("/", (_, res) => res.json(console.log(`Sei UUID é: ${myuuid}`)));
 
-app.get("/", (req, res) => ({}));
-
-app.post("/users", (req, res) => {
-  co;
-});
-
-app.put("users/:id", (req, res) => {
-  const { id } = req.params;
-  const { name: updatedName } = req.body;
-
-  const user = users.find((user) => user.id === Number(id));
-
-  user.name = updatedName;
-
-  if (!user) {
-    return res.status(404).json({
-      message: "Usuário não encontrado!",
-    });
-  }
-});
-
-app.listen(port, () => console.log("Servidor rodando na porta 8080"));
+//Rodando o servidor!
+const port = 3333;
+app.listen(port, () => console.log("Servidor rodando na porta 3333"));
