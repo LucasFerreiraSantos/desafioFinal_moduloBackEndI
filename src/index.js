e3fddc17c187ed9a8e73eb61bafaa6c846b0f10c
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-22 21:11:19 -0300

    feito o create user

--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 
 const app = express();
-let myuuid = uuidv4();
 
 //Convertendo o EXPRESS em formato JSON
 app.use(express.json());
 
-app.get("/", (_, res) => res.json(console.log(`Sei UUID é: ${myuuid}`)));
+//ARRAY DE USUÁRIOS
+const users = [];
+
+//CRIAR O USUÁRIO
+app.post("/signUp", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const checkEmail = users.find(user => user.email === email)
+
+  if(checkEmail){
+    return res.status(400).json({
+      message: "E-mail já foi cadastrado!"
+    })
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+
+  users.push(newUser);
+
+  res.status(201).json({
+    message: "Usuário criado com sucesso.", user: newUser
+  })
+});
 
 //Rodando o servidor!
 const port = 3333;
