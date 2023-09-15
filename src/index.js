ee7a122d77e62578bfa159f2d2df1280714e237c
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-09-15 20:14:12 -0300

    adicionado o Cors a aplicação

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 import express from "express";
 import users from "./routes/user";
 import scrap from "./routes/scrap";
+const cors = require("cors");
 
 const app = express();
 
 //Convertendo o EXPRESS em formato JSON
 app.use(express.json());
+app.use(cors());
 
 app.use("/users/", users);
 app.use("/scrap", scrap);
